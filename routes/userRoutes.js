1d91b47e4a36468da7649479168af27f8127a04e
dev-sydney <sydneyotutey279@gmail.com>
2023-05-21 22:03:19 +0000

    Inlcuded the route for handling requests to reset passwords after forgetting them

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,6 @@ router.route('/login').post(authController.loginUser);
 router.route('/signup').post(authController.signupUser);
 
 router.route('/forgot-password').post(authController.forgotPassword);
+router.patch('/reset-password/:resetToken', authController.resetPassword);
+
 module.exports = router;
