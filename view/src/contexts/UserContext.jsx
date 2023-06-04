f9215869d8a165c396201d73362fa0bbff1deef5
dev-sydney <sydneyotutey279@gmail.com>
2023-06-04 01:33:45 +0000

    Implemented the function that makes requests to the API to register new users

--- view/src/contexts/UserContext.jsx
+++ view/src/contexts/UserContext.jsx
@@ -9,6 +9,7 @@ export const UserContextProvider = ({ children }) => {
   const intialState = {
     user: JSON.parse(localStorage.getItem('DDS_USER')),
     isLoggedIn: false,
+    signUpSuccessMessage: null,
   };
 
   const [state, dispatch] = useReducer(userReducer, intialState);
@@ -50,12 +51,40 @@ export const UserContextProvider = ({ children }) => {
       console.log(err);
     }
   };
+  /**
+   * Function for making request to the API to register a new user into the application
+   * @param {*} formData
+   */
+  const registerUser = async (formData) => {
+    try {
+      //TODO: Make the request to the API
+      const res = await fetch('/api/v1/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      //TODO: Get the JSON data from the response
+      const results = await res.json();
+      //TODO: Dispatch to the reducer to set the context state
+      dispatch({
+        type: 'SIGN_UP',
+        payload: results.message,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <userContext.Provider
       value={{
         user: state.user,
+        signUpSuccessMessage: state.signUpSuccessMessage,
         loginUser,
+        registerUser,
       }}
     >
       {children}
