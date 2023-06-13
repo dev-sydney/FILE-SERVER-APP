9eb318ccae2efa1f099123273e6bb386d2bd4012
dev-sydney <sydneyotutey279@gmail.com>
2023-06-13 15:50:48 +0000

    Created this page component to make requests to the API to search for files

---
+++ view/src/pages/Explore.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import FeedItem from '../components/user-feed/FeedItem';
+/**
+ *
+ * @returns The Explore Page Component for searching for files
+ */
+const Explore = () => {
+  const [searchField, setSearchField] = useState('title');
+  const [searchResults, setSearchResults] = useState(null);
+  const [typingTimeout, setTypingTimeout] = useState('');
+
+  const onChange = (e) => {
+    clearTimeout(typingTimeout);
+
+    const timeout = setTimeout(() => {
+      if (e.target.value !== '') {
+        fetch(
+          `/api/v1/files/search/?fields=title,file_description,file_id,file_type,file_name&searchField=${searchField}`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify({ searchVal: e.target.value }),
+          }
+        )
+          .then((res) => res.json())
+          .then((results) => setSearchResults(results.searchResults));
+      }
+    }, 800);
+
+    setTypingTimeout(timeout);
+  };
+  return (
+    <div>
+      <form>
+        <input type="text" name="searchVal" onChange={onChange} />
+        <label htmlFor="">Search by:</label>
+        <select
+          onChange={(e) => {
+            setSearchField(e.target.value);
+          }}
+        >
+          <option value="tile">Title</option>
+          <option value="file_description">Description</option>
+        </select>
+      </form>
+      <div>
+        {searchResults &&
+          (searchResults.length > 0
+            ? searchResults.map((file) => (
+                <FeedItem file={file} key={file.file_id} />
+              ))
+            : 'No results found :(')}
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
