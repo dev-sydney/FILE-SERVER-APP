611ebe755eaa826902c3826f5a4693c200182d95
dev-sydney <sydneyotutey279@gmail.com>
2023-06-30 20:06:59 +0000

    Changed the entry point into the application

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
 
 const errorController = require('./controllers/errorController');
 
@@ -10,6 +11,7 @@ const fileRouter = require('./routes/fileRoutes');
 const clientRouter = require('./routes/clientsRoutes');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.enable('trust proxy');
 
@@ -27,4 +29,6 @@ app.use('/api/v1/clients', clientRouter);
 
 app.use(errorController);
 
-module.exports = app;
+app.listen(port, function () {
+  console.log(`Listening to requests on ${port}`);
+});
